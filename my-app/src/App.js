fa8496450d9f9eb604a1bdc7f7730daac2aa8e34
unknown <aelise17264@gmail.com>
2020-09-01 13:38:28 -0600

    rending my github usercard on site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+
+class App extends React.Component {
+  state ={
+    users: [],
+    newCard: '',
+    error: ''
+  };
+  
+componentDidMount(){
+  axios.get('https://api.github.com/users/aelise17264')
+  .then(res => {
+    console.log(res.data)
+    this.setState({
+      users: res.data
+    })
+  })
+  .catch (error =>
+    console.log('error'))
+}
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Github User Cards</h1>
+    <div className='cardMaker'>
+      <h3 className="userName">{this.state.users.name}</h3>
+      <img src={this.state.users.avatar_url}/>
+  <p>Bio: {this.state.users.bio}</p>
+  <p>Location: {this.state.users.location}</p>
+  <p>Twitter Handle: {this.state.users.twitter_username}</p>
+  <p>Followers: {this.state.users.followers}</p>
+    </div>
+
     </div>
   );
 }
-
+}
 export default App;
